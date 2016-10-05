cd0fa6c53bad9ae4135fce881171b6257557cb2d
Sean Dow Miller <seandowmiller@gmail.com>
2016-10-05 11:30:05 -0700

    expectations and reality

--- drawingproject.js
+++ drawingproject.js
@@ -25,3 +25,37 @@ canvas.beginPath();
 canvas.rect(570, 225, 20, 15);
 canvas.fillStyle = "BurlyWood";
 canvas.fill();
+
+canvas.beginPath();
+canvas.arc(573, 225, 5, 0, 2*Math.PI);
+canvas.fillStyle = "BurlyWood";
+canvas.fill();
+
+canvas.beginPath();
+canvas.arc(587, 225, 5, 0, 2*Math.PI);
+canvas.fillStyle = "BurlyWood";
+canvas.fill();
+
+canvas.beginPath();
+canvas.arc(407, 215, 20, 0, 2*Math.PI);
+canvas.fillStyle = "orange";
+canvas.fill();
+
+canvas.beginPath();
+canvas.moveTo(380,200);
+canvas.lineTo(380,215);
+canvas.lineTo(450,215);
+canvas.fillStyle = "orange";
+canvas.fill();
+
+canvas.beginPath();
+canvas.moveTo(400,204);
+canvas.lineTo(400,224);
+canvas.lineTo(470,224);
+canvas.fillStyle = "BurlyWood";
+canvas.fill();
+
+canvas.beginPath();
+canvas.rect(395, 230, 25, 60);
+canvas.fillStyle = "DarkOliveGreen";
+canvas.fill();
